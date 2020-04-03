2fcfef54d1d2fe3a9a211238ddd1e768e9ca007a
snortjs <35138382+snortjs@users.noreply.github.com>
2020-04-03 15:27:34 +0200

    Update Posts.vue

--- 303/wa-frontend/src/views/Posts.vue
+++ 303/wa-frontend/src/views/Posts.vue
@@ -11,8 +11,6 @@
 import _ from 'lodash'
 import InstagramCard from "@/components/InstagramCard.vue";
 import store from "@/store.js";
-import {test, Posts} from '@/services';
-
 
 export default {
   data() {
@@ -32,17 +30,28 @@ export default {
   methods: {
     async fetchPosts(term) {
       term = term || store.searchTerm
-      this.cards=await Posts.getAll(term)
-      /*fetch(`http://localhost:3000/posts?title=${term}`)
-        .then(response => {
-          return response.json()
-        })
-        .then(data => {
+      let pretraga=async(term)=>{
+        let response=await fetch(`http://localhost:3000/posts?title=${term}`)
+        let data=await response.json()
+        return data  
+      }
+      let terms=term.split(" ")
+      console.log(terms)
+      let promises=terms.map(e=> pretraga(e)) //ispis svih polja (promise)
+      console.log("promises",promises)
+      let results=await Promise.all(promises) //ispis sta se nalazi u svim poljima
+      console.log("results",results)
+      let rezultat=results.reduce((final,e)=>final.concat(e), []) //spoji sva polja u jedno polje (lista)
+      console.log(rezultat)
+      let data=rezultat 
+      let finalni={}
+      data.forEach(post=>finalni[post.id]=post)
+      console.log(finalni)
+      data=Object.values(finalni) //vrati samo vrijednosti iz objekta u listu
           console.log("Podaci s backenda", data)
           this.cards = data.map(doc => {
             return {id: doc.id, url: doc.source, email: doc.createdBy, title: doc.title, posted_at: Number(doc.postedAt)}
           })
-        })*/
     },
     gotoDetails(card) {
       this.$router.push({path: `post/${card.id}`})
